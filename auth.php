d82c7f5967a7fb45547dbdcf3694fd2ca6ebf0de
ErmalCahanaj <ec51206@ubt-uni.net>
2022-01-28 15:10:45 +0100

    shtimi i auth edhe db

---
+++ auth.php
@@ -0,0 +1,7 @@
+<?php
+session_start(); //creates a session or resumes the current one based on a session identifier passed via a GET or POST request
+if(!isset($_SESSION["username"])){ //The isset() function checks whether a variable is set, which means that it has to be declared
+     and is not NULL
+header("Location: login.php"); //The header() function is an inbuilt function in PHP which is used to send a raw HTTP header
+exit(); }
+?>
